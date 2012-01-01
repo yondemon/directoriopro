b49885baa9259d59221fe4e1351e4a9e25669586
Miquel Camps Orteza <gafeman@gmail.com>
2012-01-01 21:39:30 +0100

    fulltime + freelance home - colaboraciones

--- src/Application/AnunciosBundle/Controller/PostController.php
+++ src/Application/AnunciosBundle/Controller/PostController.php
@@ -46,7 +46,7 @@ class PostController extends Controller
 		$query = $em->createQueryBuilder();
 		$query->add('select', 'p')
 		   ->add('from', 'ApplicationAnunciosBundle:Post p')
-		   ->add('where', 'p.type = 0')
+		   ->add('where', 'p.type != 2')
 		   ->add('orderBy', 'p.featured DESC, p.id DESC');
 		
 		// categoria?
