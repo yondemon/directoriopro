b466cf8f8ea3853dc28a6b643db5039e37d6cd47
Miquel Camps Orteza <gafeman@gmail.com>
2012-02-05 04:48:18 +0100

    test

--- web/bundles/applicationanuncios/js/general.js
+++ web/bundles/applicationanuncios/js/general.js
@@ -1,3 +1,10 @@
+function replaceURLWithHTMLLinks(text) {
+    var exp = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
+    return text.replace(exp,"<a href='$1' target='_blank'>$1</a>"); 
+}
+
+
+
 /* dribbble */
 
 var dribbble_load = false;
